e10f17aa57e3fa2a6888883edbe2d76875b46a32
Hugo <hugogiltejero@gmail.com>
2025-01-16 00:21:47 +0100

    refactor(index): añado css al index

---
+++ script.js
@@ -0,0 +1,4 @@
+const firefox = document.querySelector('#firefox');
+firefox.addEventListener('click', () => {
+    window.location.href = 'https://addons.mozilla.org/es/firefox/addon/keyboard-recorder/';
+});;
